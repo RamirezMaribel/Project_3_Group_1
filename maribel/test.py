0f47fc5131b5ad1884bedced92667789767fde1d
RamirezMaribel <ramirezmmaribel@gmail.com>
2024-07-18 00:35:28 -0700

    updated ipynb file

--- maribel/test.py
+++ maribel/test.py
@@ -1,5 +1,17 @@
+# Dependencies
+
 import pypokedex
+import requests
+import pandas as pd
+import json
+
 
 pokemon = pypokedex.get(dex=1)  # DEX must be a valid _national_ pokedex
                                   # number
 print(pokemon)
+
+#Type table creation
+
+
+
+
