dc5a9d4f7770eb7bad7bf2ead07710a665575f93
aocanoni <157078630+aocanoni@users.noreply.github.com>
2024-07-23 04:39:55 -0700

    uploaded pokemon_db_query.sql and updated code in pokemonData.ipynb

--- pokemon_db_query.sql
+++ pokemon_db_query.sql
@@ -20,8 +20,8 @@ CREATE TABLE Pokemon (
 -- Maribel
 CREATE TABLE Type (
     type_name VARCHAR(50) NOT NULL,
-    weak_to VARCHAR(50) NOT NULL,
-    strong_against VARCHAR(50) NOT NULL,
+    weak_to VARCHAR(255),
+    strong_against VARCHAR(255),
     PRIMARY KEY (type_name)
 );
 -- https://pokeapi.co/api/v2/ability?limit=367
@@ -29,8 +29,8 @@ CREATE TABLE Type (
 CREATE TABLE Ability (
     ability_id int NOT NULL,
     ability_name VARCHAR(255) NOT NULL,
-    effect VARCHAR(255) NOT NULL,
-    short_effect VARCHAR(255) NOT NULL,
+    effect VARCHAR(2000) NOT NULL,
+    short_effect VARCHAR(2000) NOT NULL,
     PRIMARY KEY (ability_id),
 	UNIQUE (ability_name)
 );
@@ -59,12 +59,12 @@ CREATE TABLE pokeGame (
 CREATE TABLE Move (
     move_id int NOT NULL,
     move_name VARCHAR(255) NOT NULL,
-    move_effect VARCHAR(255) NOT NULL,
-    move_power int NOT NULL,
-    move_pp int NOT NULL,
-    move_acc int NOT NULL,
-    move_type VARCHAR(50) NOT NULL,
-    dmg_class VARCHAR(50) NOT NULL,
+    move_effect VARCHAR(5000),
+    move_power int,
+    move_pp int,
+    move_acc int,
+    move_type VARCHAR(50),
+    dmg_class VARCHAR(50),
     PRIMARY KEY (move_id)
 );
 CREATE TABLE pokeMove (
@@ -95,3 +95,68 @@ CREATE INDEX idx_Game_game_name
 ON Game (game_name);
 CREATE INDEX idx_Move_move_name
 ON Move (move_name);
+
+
+-- Easier upload of csvs- delete the first '/*' which is commenting out the code
+-- Run in order *REMEMBER TO UPDATE EACH TO YOUR ABSOLUTE FILE PATH:
+-- Example: '\Users\username\OneDrive\Desktop\folder\folder2\all\pokeGame.csv'
+
+/*
+COPY Game (game_id, game_name)
+FROM 'all\pokeGame.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY pokeEvo (evolution_chain, stage_1, stage_2, stage_3)
+FROM 'all\pokeEvo.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Type (type_name, weak_to, strong_against)
+FROM 'all\type.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Pokemon (poke_id, poke_name, type, second_type, base_hp, base_att, base_sp_atk, base_def, base_sp_def, base_spd, evolution_chain)
+FROM 'all\pokeInfo.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Ability (ability_id, ability_name, effect, short_effect)
+FROM 'all\abilityEffects.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY pokeAbility (poke_id, ability_name)
+FROM 'all\pokeAbilities.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY pokegame(poke_id, game_id)
+FROM 'all\pokeGameIDS.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY Move(move_id, move_name, move_effect, move_power, move_acc, move_pp, move_type, dmg_class)
+FROM 'all\moves.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY pokeMove(move_id, poke_id)
+FROM 'all\pokeMoves.csv'
+DELIMITER ','
+CSV HEADER;
+*/
+
+-- drop tables code (perform in order)
+/*
+drop table pokemove;
+drop table pokegame;
+drop table pokeability;
+drop table move;
+drop table game;
+drop table ability;
+drop table pokemon;
+drop table pokeevo;
+drop table type;
+*/
